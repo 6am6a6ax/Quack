f49232be6ab74e6a90f80177a876f4a570488643
6am6a6ax <bujhmjktujdbx@gmail.com>
2022-10-07 18:15:54 +0300

    Added new glm types aliases

--- engine/include/quack/math/math.h
+++ engine/include/quack/math/math.h
@@ -8,13 +8,30 @@
 #include <glm/gtx/quaternion.hpp>
 
 namespace Quack {
-    using Vector1f = glm::vec1;
     using Vector2f = glm::vec2;
     using Vector3f = glm::vec3;
     using Vector4f = glm::vec4;
 
+    using Vector2i = glm::ivec2;
+    using Vector3i = glm::ivec3;
+    using Vector4i = glm::ivec4;
+
+    using Vector2u = glm::uvec2;
+    using Vector3u = glm::uvec3;
+    using Vector4u = glm::uvec4;
+
+    using Mat2f = glm::mat2;
     using Mat3f = glm::mat3;
     using Mat4f = glm::mat4;
+
+    using Mat2x3f = glm::mat2x3;
+    using Mat3x2f = glm::mat3x2;
+
+    using Mat2x4f = glm::mat2x4;
+    using Mat4x2f = glm::mat4x2;
+
+    using Mat3x4f = glm::mat3x4;
+    using Mat4x3f = glm::mat4x3;
 }
 
 #endif
