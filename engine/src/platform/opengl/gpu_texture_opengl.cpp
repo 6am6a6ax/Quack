3832218e49bfc77a40a6ca04ce4f42e88d0dffd2
6am6a6ax <bujhmjktujdbx@gmail.com>
2022-12-10 03:52:05 +0300

    Fixed include path

--- engine/src/platform/opengl/gpu_texture_opengl.cpp
+++ engine/src/platform/opengl/gpu_texture_opengl.cpp
@@ -1,7 +1,7 @@
 #include "quack/quack.h"
 
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <stb/stb_image.h>
 
 Quack::GPUTextureOpenGL::GPUTextureOpenGL(const Quack::GPUTextureDescription & desc) : GPUTexture(desc) {
     int width, height, channels;
